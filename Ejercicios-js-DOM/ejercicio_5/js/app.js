bee581fb0a551a58e03453affd2d3c6fe7756060
Jesus Rojas <jesusnicotra@gmail.com>
2023-06-27 20:46:08 -0400

    trabajo to do list

--- Ejercicios-js-DOM/ejercicio_5/js/app.js
+++ Ejercicios-js-DOM/ejercicio_5/js/app.js
@@ -3,14 +3,14 @@ const agregar = document.querySelector('#agregar');
 const borrar = document.querySelector('#borrar');
 const lista = document.querySelector('ol');
 const mensajeError = document.querySelector('#error');
-const listaTarea = [];
+/* const listaTarea = []; */
 
 agregar.addEventListener('click', () => {
   const tarea = input.value;
   
   if (tarea !== '') {
 
-    listaTarea.push(tarea);
+   /*  listaTarea.push(tarea); */
     const elementoLista = document.createElement('li');
     elementoLista.textContent = tarea;
     lista.appendChild(elementoLista);
@@ -19,13 +19,13 @@ agregar.addEventListener('click', () => {
     mensajeError.textContent = '';
     
     } else { 
-
+    
     mensajeError.textContent = 'Debe ingresar una tarea!!'
   }
 });
 
 borrar.addEventListener('click', () => {
-  listaTarea.length = 0;
+/*   listaTarea.length = 0; */
   lista.innerHTML = '';
   mensajeError.textContent = '';
 });
